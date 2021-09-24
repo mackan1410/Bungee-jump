83a2758624fcff2d275d456de42e195af705f06f
mackan1410 <32570209+mackan1410@users.noreply.github.com>
2021-09-24 19:18:10 +0200

    Update bungeemodel.m

--- bungeemodel.m
+++ bungeemodel.m
@@ -1,13 +1,10 @@
-%{
-k = 12N/m
-m = 80kg
-luftmotstånd koefficient C = 0.24kg/m
-g = 9.82m/s^2
-%}
-
 function out = bungeemodel(t, x)
-if x(2) < 0
-    out = [x(2); -0.15*x(1)+0.003*(x(2)^2)];
-else
-    out = [x(2); -0.15*x(1)-0.003*(x(2)^2)];
+    g = 9.82;
+    m = 80;
+    k = 80;
+    C = 0.24;
+    if x(1) < 0
+        out = [x(2); g-(C/m)*x(2)*abs(x(2))];
+    else 
+        out = [x(2); g-(k/m)*x(1)-(C/m)*x(2)*abs(x(2))];
 end
