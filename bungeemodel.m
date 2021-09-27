1fb1fd06255efff67c53ce1c0c5a27e7502d3ac9
cawe <carl@willman.nu>
2021-09-27 14:15:29 +0200

    tog bort negativ hastighet för frittfall

--- bungeemodel.m
+++ bungeemodel.m
@@ -4,7 +4,7 @@ function out = bungeemodel(t, x)
     k = 80;
     C = 0.24;
     if x(1) < 0
-        out = [x(2); g-(C/m)*x(2)*abs(x(2))];
+        out = [x(2); g-(C/m)*x(2)*x(2)];
     else 
         out = [x(2); g-(k/m)*x(1)-(C/m)*x(2)*abs(x(2))];
 end
